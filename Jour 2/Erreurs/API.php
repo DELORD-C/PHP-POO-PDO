0b4610b9edc376ba2868e1b4f87eac975c1260ca
Clément <clement.delord@epitech.eu>
2022-09-13 10:25:00 +0200

    API

---
+++ Jour 2/Erreurs/API.php
@@ -0,0 +1,11 @@
+<?php
+
+$response = file_get_contents(
+    'https://api.themoviedb.org/3/movie/76341?api_key=625b3e1220c0fca7c7ac7f6fcca786ac'
+);
+
+$decode = json_decode($response);
+
+echo "<pre>";
+var_dump($decode);
+echo "</pre>";
