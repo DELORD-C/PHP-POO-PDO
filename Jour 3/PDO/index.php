0c41ecfa244973bf88b92aa7e7f8ff84566ffce5
Clément <clement.delord@epitech.eu>
2022-09-14 10:46:19 +0200

    pdo connect

--- Jour 3/PDO/index.php
+++ Jour 3/PDO/index.php
@@ -1,2 +1,14 @@
 <?php
 
+//On récupère les données du fichier env.json pour en faire un objet
+$env = json_decode(file_get_contents('env.json'));
+
+$db = new PDO(
+    $env->database->type . ':host=' . $env->database->host . ';dbname=' . $env->database->dbname,
+    $env->database->user,
+    $env->database->password
+);
+//On se connecte à la base de donnée en utilisant les paramètres de notre fichier env.json
+
+//On créé notre requête
+$query = $db->prepare("SELECT * FROM agents;");
