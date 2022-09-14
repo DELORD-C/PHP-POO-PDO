7d195d5a895ed541841542c8785e136081f6a099
Clément <clement.delord@epitech.eu>
2022-09-14 11:35:26 +0200

    Insert

--- Jour 3/PDO/index.php
+++ Jour 3/PDO/index.php
@@ -12,3 +12,31 @@ $db = new PDO(
 
 //On créé notre requête
 $query = $db->prepare("SELECT * FROM agents;");
+//On exécute la requête
+$query->execute();
+//On récupère les résultats
+$results = $query->fetchAll();
+
+//On récupère un seul résultat
+// $results = $query->fetch();
+
+// echo '<pre>';
+// var_dump($results);
+// echo '</pre>';
+
+$number = 'A017';
+$name = 'Jean';
+$zone = 'Dunkerk';
+$commission = 0.10;
+$phone = '+3245698745';
+$country = 'Belgium';
+
+$query = $db->prepare("INSERT INTO agents VALUES (:number, :name, :zone, :commission, :phone, :country);");
+//les pseudos variable (:number, :name etc...) doivent être populées par la fonction bindParam
+$query->bindParam('number', $number);
+$query->bindParam('name', $name);
+$query->bindParam('zone', $zone);
+$query->bindParam('commission', $commission);
+$query->bindParam('phone', $phone);
+$query->bindParam('country', $country);
+$query->execute();
